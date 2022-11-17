dadc1c2242b374e49b7920394ed63c4441d715a2
Apon wuz here <frost_bird@hotmail.com>
2022-11-17 13:35:03 -0600

    burninating the countryside

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,35 +1,32 @@
 import { gql } from "@apollo/client";
 
 export const LOGIN_USER = gql`
-    mutation login($username: String!, $password: String!) {
-        login(username: $username, password: $password) {
-            token
-            user {
-                _id
-                username
-                email
-                password
-            }
-        }
-    }`;
+  mutation login($username: String!, $password: String!) {
+    login(username: $username, password: $password) {
+      token
+      user {
+        _id
+      }
+    }
+  }
+`;
 
 export const ADD_USER = gql`
-    mutation addUser($username: String!, $email: String!, $password: String!) {
-        addUser(username: $username, email: $email, password: $password) {
-            token
-            user {
-                _id
-                username
-                email
-                password
-            }
-        }
-    }`;
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+      }
+    }
+  }
+`;
 
 export const ADD_MOOD = gql`
-    mutation addMood($userId: String!, $mood: Int!) {
-        addMood(userId: $userId, mood: $mood) {
-            _id
-            mood
-        }
-    }`;
+  mutation addMood($userId: String!, $mood: Int!) {
+    addMood(userId: $userId, mood: $mood) {
+      _id
+      mood
+    }
+  }
+`;
