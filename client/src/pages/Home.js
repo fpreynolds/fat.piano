b7f0c9f24a8fa00650709a2041ba764e6cf2670f
abarrow1016 <annabarrowmusic@gmail.com>
2022-11-22 19:21:14 -0600

    added image to home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,15 @@
 import React from "react";
+import raccoon from "../assets/raccoon.png";
 
 const Home = () => {
   return (
     <div className="container">
       <h1>Oh Hi</h1>
+      <div className="img1">
+        <img src={raccoon} alt="gasping raccoon"/>
+      </div>
     </div>
+    
   );
 };
 
